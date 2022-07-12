01dbe0ea4d806364522f6b67a0eac71d0222ae99
Jose <najo1519@gmail.com>
2022-07-12 10:33:17 -0500

    nuevotrabajo

--- trabajo.py
+++ trabajo.py
@@ -1,12 +1,10 @@
 from array import array
 from multiprocessing.dummy import Array
 
-import numpy as np
-
 for i in range(1,5):
     print(i)
 
     #no entiendo que tanta verga con esto
 A = array([[2,1,5],[2,4,1]])
 
-print(A)
+print(A) 
